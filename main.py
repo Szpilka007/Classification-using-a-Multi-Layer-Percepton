0ba1ed8da76b57ac610a520ea67c971a313e2577
Szpila007 <38570359+Szpila007@users.noreply.github.com>
2019-04-13 16:01:13 +0200

    Added function to read a file , and added to control a file with data

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("First commit")
+import pylab
+import numpy
+import matplotlib as plt
+
+
+def readFile(file,amountOfData):
+    try:
+        table = []
+        with open(file, "r") as f:
+            lista_linii = [line.rstrip("\n") for line in f]
+            for linia in lista_linii:
+                for pole in linia.split(","):
+                    table.append(pole)
+        ir = [[]]
+        counter_2 = 0
+        counter = 0
+        while counter < len(table):
+            ir.append([])
+            for x in range(amountOfData):
+                ir[counter_2].append(table[counter])
+                counter += 1
+            counter_2 += 1
+        for x in ir:
+             print(x)
+        return ir
+
+
+    except FileNotFoundError:
+        print("File not found")
+
+
+#Execuate Code
+readFile("Iris.txt",5)
